a1c9a30155026d953aa1f80768ad7959ac5e97b8
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2023-08-15 13:03:14 +0100

    setup savings-plan model and migrate

---
+++ models/savings_plan.js
@@ -0,0 +1,31 @@
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const SavingsPlan = sequelize.define("SavingsPlan", {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    startingAmount: {
+      type: DataTypes.FLOAT,
+    },
+    frequency: {
+      type: DataTypes.ENUM("daily", "weekly", "bi-weekly", "monthly"),
+      defaultValue: "monthly",
+    },
+    savingMethod: {
+      type: DataTypes.ENUM("manual", "automatic"),
+      defaultValue: "manual",
+    },
+    planLength: {
+      type: DataTypes.INTEGER,
+    },
+    user_id: { type: DataTypes.INTEGER, allowNull: false },
+  });
+  return SavingsPlan;
+};
